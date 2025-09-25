3243453ee7a5f9cbfa9d94bc895eb20c773bc0a4
Darmanin96 <darmanindaniel@hotmail.es>
2025-09-26 00:28:24 +0100

    Update

---
+++ js/config.js
@@ -0,0 +1,26 @@
+function crearTabla(datos){
+    const tbody = document.querySelector("#tabla-gastos tbody");
+    tbody.innerHTML = ""; 
+
+    datos.forEach(dato => {
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+            <td>${dato.nombreTecnico}</td>
+            <td>${dato.codigoEmpleado}</td>
+            <td>${dato.delegacion}</td>
+            <td>${dato.importe}</td>
+            <td>${dato.fecha}</td>
+            <td>
+                ${dato.imagenAlimento 
+                    ? `<img src="data:image/jpeg;base64,${dato.imagenAlimento}" alt="Alimento" width="100">`
+                    : ''}
+            </td>
+            <td>
+                ${dato.imagenTicket 
+                    ? `<img src="data:image/jpeg;base64,${dato.imagenTicket}" alt="Ticket" width="100">`
+                    : ''}
+            </td>
+        `;
+        tbody.appendChild(fila);
+    });
+}
--- js/request.js
+++ js/request.js
@@ -1,10 +1,11 @@
 function getAllDatos() {
     $.ajax({
-        url: "http://localhost/Api-Tecnicos/gastos.php",
+        url: "http://localhost/Api-Tecnicos/gastos",
         type: "GET",
         dataType: "json",
         success: function(datos) {
             console.log(datos);
+            crearTabla(datos.data);
         },
         error: function(xhr, status, error) {
             console.error("Error en la solicitud:", status, error);
@@ -12,3 +13,5 @@ function getAllDatos() {
     });
 }
 
+
+getAllDatos();
